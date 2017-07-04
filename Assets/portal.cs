312418fdab20e9404700b8972980545db99668c7
xushuxu <xuwuxiu@gmail.com>
2017-07-04 14:27:20 +0800

    Initial commit

---
+++ Assets/portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour {
+
+    public Transform Point;
+    // Use this for initialization
+    void Start () {
+
+    }
+    internal protected void OnTriggerEnter(Collider other)
+    {
+        other.transform.root.position = Point.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+}
